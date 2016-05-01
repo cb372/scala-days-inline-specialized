b98c1eedaddb770078c06a1e8cb1c247aa7a386b
Chris Birchall <chris.birchall@gmail.com>
2016-05-01 15:23:46 +0100

    Fix FFT benchmark

--- inlining/FFTBenchmark.scala
+++ inlining/FFTBenchmark.scala
@@ -3,6 +3,7 @@ package benchmark.fft
 import scala.math._
 import scala.annotation._
 import org.openjdk.jmh.annotations._
+import org.openjdk.jmh.infra.Blackhole
 import java.util.concurrent.TimeUnit
 
 final case class ComplexWithInline(r: Double, i: Double) {
@@ -27,68 +28,61 @@ final case class ComplexWithNoInline(r: Double, i: Double) {
 @State(Scope.Thread)
 class FFTBenchmark {
 
-  val input: Seq[Double] = Seq.tabulate(32768)(_ => scala.util.Random.nextDouble)
+  val input: Seq[Double] = Seq.tabulate(65536)(_ => scala.util.Random.nextDouble)
 
-  @Benchmark
-  @BenchmarkMode(Array(Mode.AverageTime))
-  @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def fftWithInline: Seq[ComplexWithInline] = {
-    def filterByIndex[A](xs: Seq[A])(p: Int => Boolean) =
-      xs.zipWithIndex.collect { case (x, i) if p(i) => x }
+  private def filterByIndex[A](xs: Seq[A])(p: Int => Boolean) =
+    xs.zipWithIndex.collect { case (x, i) if p(i) => x }
 
-    def rec(data: Seq[ComplexWithInline]): Seq[ComplexWithInline] = {
-      data.length match {
-        case 0 => Nil
-        case 1 => data
-        case n => {
-          val evens = rec(filterByIndex(data) { _ % 2 == 0 })
-          val odds = rec(filterByIndex(data) { _ % 2 != 0 })
-          val phase = for (i <- 0 to n / 2 - 1) yield {
-            val p = -2.0 * Pi * i / n
-            ComplexWithInline(cos(p), sin(p))
-          }
-
-          val ops = (odds, phase).zipped map { _ * _ }
-          val one = (evens, ops).zipped map { _ + _ }
-          val two = (evens, ops).zipped map { _ - _ }
-
-          one ++ two
-        }
+  def _fftWithInline(data: Seq[ComplexWithInline]): Seq[ComplexWithInline] = data.length match {
+    case 0 => Nil
+    case 1 => data
+    case n =>
+      val evens = _fftWithInline(filterByIndex(data) { _ % 2 == 0 })
+      val odds = _fftWithInline(filterByIndex(data) { _ % 2 != 0 })
+      val phase = for (i <- 0 to n / 2 - 1) yield {
+        val p = -2.0 * Pi * i / n
+        ComplexWithInline(cos(p), sin(p))
       }
-    }
 
-    rec(input map { a => ComplexWithInline(a, 0.0) })
+      val ops = (odds, phase).zipped map { _ * _ }
+      val one = (evens, ops).zipped map { _ + _ }
+      val two = (evens, ops).zipped map { _ - _ }
+
+      one ++ two
   }
 
   @Benchmark
   @BenchmarkMode(Array(Mode.AverageTime))
   @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  def fftWithNoInline: Seq[ComplexWithNoInline] = {
-    def filterByIndex[A](xs: Seq[A])(p: Int => Boolean) =
-      xs.zipWithIndex.collect { case (x, i) if p(i) => x }
+  def fftWithInline(bh: Blackhole): Unit = {
+    val result = _fftWithInline(input map { a => ComplexWithInline(a, 0.0) })
+    bh.consume(result)
+  }
 
-    def rec(data: Seq[ComplexWithNoInline]): Seq[ComplexWithNoInline] = {
-      data.length match {
-        case 0 => Nil
-        case 1 => data
-        case n => {
-          val evens = rec(filterByIndex(data) { _ % 2 == 0 })
-          val odds = rec(filterByIndex(data) { _ % 2 != 0 })
-          val phase = for (i <- 0 to n / 2 - 1) yield {
-            val p = -2.0 * Pi * i / n
-            ComplexWithNoInline(cos(p), sin(p))
-          }
-
-          val ops = (odds, phase).zipped map { _ * _ }
-          val one = (evens, ops).zipped map { _ + _ }
-          val two = (evens, ops).zipped map { _ - _ }
-
-          one ++ two
-        }
+  def _fftWithNoinline(data: Seq[ComplexWithNoInline]): Seq[ComplexWithNoInline] = data.length match {
+    case 0 => Nil
+    case 1 => data
+    case n =>
+      val evens = _fftWithNoinline(filterByIndex(data) { _ % 2 == 0 })
+      val odds = _fftWithNoinline(filterByIndex(data) { _ % 2 != 0 })
+      val phase = for (i <- 0 to n / 2 - 1) yield {
+        val p = -2.0 * Pi * i / n
+        ComplexWithNoInline(cos(p), sin(p))
       }
-    }
 
-    rec(input map { a => ComplexWithNoInline(a, 0.0) })
+      val ops = (odds, phase).zipped map { _ * _ }
+      val one = (evens, ops).zipped map { _ + _ }
+      val two = (evens, ops).zipped map { _ - _ }
+
+      one ++ two
+  }
+
+  @Benchmark
+  @BenchmarkMode(Array(Mode.AverageTime))
+  @OutputTimeUnit(TimeUnit.MICROSECONDS)
+  def fftWithNoInline(bh: Blackhole): Unit = {
+    val result = _fftWithNoinline(input map { a => ComplexWithNoInline(a, 0.0) })
+    bh.consume(result)
   }
 
 }
